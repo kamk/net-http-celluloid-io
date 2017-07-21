7c1f2808a6b2ad626c035746d8a47c6407f8a033
Kamil Kukura <kamk@pentatri.cz>
2017-07-21 13:54:13 +0200

    Synchronize connect method with ruby 2.3 - handle read timeout

--- lib/net/http/celluloid_io.rb
+++ lib/net/http/celluloid_io.rb
@@ -9,7 +9,11 @@ class Net::HTTP::CelluloidIO < Net::HTTP
     def self.timeout(sec=nil, err_class=nil, &blk)
       if Celluloid.actor?
         if sec
-          Celluloid.timeout(sec, &blk)
+          begin
+            Celluloid.timeout(sec, &blk)
+          rescue Celluloid::TaskTimeout => e
+            raise err_class.new(e)
+          end
         else
           blk.call
         end
@@ -23,14 +27,17 @@ class Net::HTTP::CelluloidIO < Net::HTTP
     end
   end
 
+
   class BufferedIO < Net::BufferedIO
     include Timeout
 
     def rbuf_fill
-      #TODO: trap @read_timeout.
-      @rbuf << @io.readpartial(BUFSIZE)
+      Timeout.timeout(@read_timeout, Net::ReadTimeout) do
+        @rbuf << @io.readpartial(BUFSIZE)
+      end
     end
   end
+
 
   include Timeout
 
@@ -45,6 +52,7 @@ class Net::HTTP::CelluloidIO < Net::HTTP
     end
   end
   
+
   def connect_celluloid
     if proxy? then
       conn_address = proxy_address
@@ -53,24 +61,33 @@ class Net::HTTP::CelluloidIO < Net::HTTP
       conn_address = address
       conn_port    = port
     end
-    
-    D "opening connection to #{conn_address}..."
-    s = timeout(@open_timeout) { ::Celluloid::IO::TCPSocket.new(conn_address, conn_port) }
+
+    D "opening connection to #{conn_address}:#{conn_port}..."
+    s = Timeout.timeout(@open_timeout, Net::OpenTimeout) {
+      begin
+        ::Celluloid::IO::TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
+      rescue => e
+        raise e, "Failed to open TCP connection to " +
+          "#{conn_address}:#{conn_port} (#{e.message})"
+      end
+    }
+    s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
     D "opened"
     if use_ssl?
       ssl_parameters = Hash.new
       iv_list = instance_variables
-      SSL_ATTRIBUTES.each do |name|
-        ivname = "@#{name}".intern
+      SSL_IVNAMES.each_with_index do |ivname, i|
         if iv_list.include?(ivname) and
-            value = instance_variable_get(ivname)
-          ssl_parameters[name] = value
+          value = instance_variable_get(ivname)
+          ssl_parameters[SSL_ATTRIBUTES[i]] = value if value
         end
       end
       @ssl_context = OpenSSL::SSL::SSLContext.new
       @ssl_context.set_params(ssl_parameters)
-      s = ::Celluloid::IO::SSLSocket.new(s.to_io, @ssl_context)
-      s.to_io.sync_close = true
+      D "starting SSL for #{conn_address}:#{conn_port}..."
+      s = ::Celluloid::IO::SSLSocket.new(s, @ssl_context)
+      s.sync_close = true
+      D "SSL established"
     end
     @socket = BufferedIO.new(s)
     @socket.read_timeout = @read_timeout
@@ -79,23 +96,42 @@ class Net::HTTP::CelluloidIO < Net::HTTP
     if use_ssl?
       begin
         if proxy?
-          @socket.writeline sprintf('CONNECT %s:%s HTTP/%s',
-                                    @address, @port, HTTPVersion)
-          @socket.writeline "Host: #{@address}:#{@port}"
+          buf = "CONNECT #{@address}:#{@port} HTTP/#{HTTPVersion}\r\n"
+          buf << "Host: #{@address}:#{@port}\r\n"
           if proxy_user
             credential = ["#{proxy_user}:#{proxy_pass}"].pack('m')
             credential.delete!("\r\n")
-            @socket.writeline "Proxy-Authorization: Basic #{credential}"
+            buf << "Proxy-Authorization: Basic #{credential}\r\n"
           end
-          @socket.writeline ''
+          buf << "\r\n"
+          @socket.write(buf)
           HTTPResponse.read_new(@socket).value
         end
         # Server Name Indication (SNI) RFC 3546
-        s.to_io.hostname = @address if s.to_io.respond_to?(:hostname=)
-        timeout(@open_timeout) { s.connect }
-        if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
-          s.to_io.post_connection_check(@address)
+        s.hostname = @address if s.respond_to? :hostname=
+        if @ssl_session and
+           Process.clock_gettime(Process::CLOCK_REALTIME) < @ssl_session.time.to_f + @ssl_session.timeout
+          s.session = @ssl_session if @ssl_session
         end
+        if timeout = @open_timeout
+          while true
+            raise Net::OpenTimeout if timeout <= 0
+            start = Process.clock_gettime Process::CLOCK_MONOTONIC
+            # to_io is required because SSLSocket doesn't have wait_readable yet
+            case s.connect_nonblock(exception: false)
+            when :wait_readable; s.to_io.wait_readable(timeout)
+            when :wait_writable; s.to_io.wait_writable(timeout)
+            else; break
+            end
+            timeout -= Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
+          end
+        else
+          s.connect
+        end
+        if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
+          s.post_connection_check(@address)
+        end
+        @ssl_session = s.session
       rescue => exception
         D "Conn close because of connect error #{exception}"
         @socket.close if @socket and not @socket.closed?
@@ -105,4 +141,3 @@ class Net::HTTP::CelluloidIO < Net::HTTP
     on_connect
   end
 end
-
